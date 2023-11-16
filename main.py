ecdefc5411b5568a8ac1c702b4faa83f7f1a4edd
carlos.cruz <tomli_lt@hotmail.com>
2023-11-16 17:11:17 +0100

    description on the new columns

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
 with col1:
     st.image("images/photo.png")
 
@@ -16,4 +17,8 @@ with col2:
     """
     st.info(content)
 
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me !
+"""
+st.write(content2)
 
